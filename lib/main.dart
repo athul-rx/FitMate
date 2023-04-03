887a9a5f2efc85dcb3f6bcf8076bed669047860d
Aswin S <aswinsaswin25@gmail.com>
2023-04-04 00:09:03 +0530

    personalDetails page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:fitmate/Screens/login_screen.dart';
+import 'package:fitmate/Screens/person_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         )
 
       ),
-      home: const LoginScreen(),
+      home: const PersonalDetails(),
     );
   }
 }
