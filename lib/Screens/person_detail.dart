9ce9e1005b718de25dea0efa34ac5dbb671cbf64
Jona Joy <jonajoy142@gmail.com>
2023-04-04 23:53:08 +0530

    first work page done!

--- lib/Screens/person_detail.dart
+++ lib/Screens/person_detail.dart
@@ -1,8 +1,6 @@
 import 'package:fitmate/Components/button.dart';
 import 'package:fitmate/widgets/TextField.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class PersonalDetails extends StatefulWidget {
